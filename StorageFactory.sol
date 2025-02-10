680bf1fbd42a36c74aa17541d7ecb117b5840428
carlyjames <abrahamjames543@gmail.com>
2025-02-10 07:57:33 +0100

    latest commit

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -3,15 +3,26 @@
 
 pragma solidity 0.8.26;
 
-import "./SimpleStorage.sol";
+import {SimpleStorage} from "./SimpleStorage.sol";
 
 contract StorageFactory {
 
     //uint256 public favoriteNumber
     // type visibility name
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public listOfSimpleStorageContracts;
     
     function createSimpleStorageContract() public  {
-        simpleStorage = new SimpleStorage();
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
+        listOfSimpleStorageContracts.push(newSimpleStorageContract);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newsimpleStorageNumber) public {
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        mySimpleStorage.store(_newsimpleStorageNumber);
+    }
+
+    function sfGet(uint _simpleStorageIndex) public view returns(uint256)  {
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        return mySimpleStorage.retrieve();
     }
 }
